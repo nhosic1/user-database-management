e9c67d14579c046020919652801b2541d13decee
nedim <https://github.com/nhosic1>
2021-01-11 17:54:02 +0100

    dodani default podaci u sql dump

--- korisnici.db.sql
+++ korisnici.db.sql
@@ -9,4 +9,8 @@ CREATE TABLE IF NOT EXISTS "korisnik" (
 	"password"	TEXT,
 	PRIMARY KEY("id")
 );
+INSERT INTO "korisnik" VALUES('1', 'Vedran', 'Ljubović', 'vljubovic@etf.unsa.ba', 'vedranlj', 'test');
+INSERT INTO "korisnik" VALUES('2', 'Amra', 'Delić', 'adelic@etf.unsa.ba', 'amrad', 'test');
+INSERT INTO "korisnik" VALUES('3', 'Tarik', 'Sijerčić', 'tsijercic@etf.unsa.ba', 'tariks', 'test');
+INSERT INTO "korisnik" VALUES('4', 'Rijad', 'Fejzić', 'rfejzic@etf.unsa.ba', 'rijadf', 'test');
 COMMIT;
