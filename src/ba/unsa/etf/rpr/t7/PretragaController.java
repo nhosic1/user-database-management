28a21524c812b2c9c5d720d2134cb3ba6ba7f333
nedim <https://github.com/nhosic1>
2021-01-17 16:20:07 +0100

    implementirana akcija za dugme Cancel

--- src/ba/unsa/etf/rpr/t7/PretragaController.java
+++ src/ba/unsa/etf/rpr/t7/PretragaController.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr.t7;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class PretragaController {
+    public void cancel(ActionEvent actionEvent) {
+        Node node = (Node)actionEvent.getSource();
+        Stage stage = (Stage)node.getScene().getWindow();
+        stage.close();
+    }
 }
