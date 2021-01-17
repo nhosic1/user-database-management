d9013bdeb63102c9ef55e6bc6e6459ca0cbe8489
nedim <https://github.com/nhosic1>
2021-01-17 22:03:12 +0100

    pretraživanje se sada vrši u odvojenoj niti
    Korisnički više interfejs nije blokiran pri pretraživanju. U folder img je dodan loading.gif

--- src/ba/unsa/etf/rpr/t7/PretragaController.java
+++ src/ba/unsa/etf/rpr/t7/PretragaController.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.t7;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -31,38 +32,53 @@ public class PretragaController {
     public void search(ActionEvent actionEvent) {
         String pretraga = fldPretraga.getText().replace(" ", "+");
         String adresa = "http://api.giphy.com/v1/gifs/search?q=" + pretraga + "&api_key=K7ZzpHSODkIauWCy4wi0qgypKMpicFX5&limit=25";
+        tilePane.getChildren().clear();
 
-        try{
-            URL url = new URL(adresa);
-            BufferedReader ulaz = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-            String json = "", line = null;
-            while ((line = ulaz.readLine()) != null) {
-                json = json + line;
+        Button loadingBtn = new Button();
+        ImageView loadingView = new ImageView(new Image("/img/loading.gif"));
+        loadingView.setFitHeight(128);
+        loadingView.setFitWidth(128);
+        loadingBtn.setGraphic(loadingView);
+        tilePane.getChildren().add(loadingBtn);
+
+        new Thread(() -> {
+
+            try{
+                URL url = new URL(adresa);
+                BufferedReader ulaz = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+                String json = "", line = null;
+                while ((line = ulaz.readLine()) != null) {
+                    json = json + line;
+                }
+                ulaz.close();
+                JSONObject obj = new JSONObject(json);
+                JSONArray jArray = obj.getJSONArray("data");
+                for(int i=0; i<jArray.length(); i++){
+                    JSONObject o = jArray.getJSONObject(i);
+                    String img = o.getJSONObject("images").getJSONObject("original_still").getString("url");
+                    int poc = img.indexOf("giphy");
+                    int kraj = img.indexOf("?");
+                    String link = "https://i." + img.substring(poc, kraj);
+                    Button b = new Button();
+                    ImageView view = new ImageView(new Image(link));
+                    view.setFitHeight(128);
+                    view.setFitWidth(128);
+                    Platform.runLater(() -> {
+                        b.setGraphic(view);
+                        tilePane.getChildren().add(0, b);
+                    });
+                }
+            }  catch(MalformedURLException e) {
+                System.out.println("String "+adresa+" ne predstavlja validan URL");
+            } catch(IOException e) {
+                System.out.println("Greška pri kreiranju ulaznog toka");
+                System.out.println(e.getMessage());
+            } catch(Exception e) {
+                System.out.println("Poteškoće sa obradom JSON podataka");
+                System.out.println(e.getMessage());
             }
-            ulaz.close();
-            JSONObject obj = new JSONObject(json);
-            JSONArray jArray = obj.getJSONArray("data");
-            for(int i=0; i<jArray.length(); i++){
-                JSONObject o = jArray.getJSONObject(i);
-                String img = o.getJSONObject("images").getJSONObject("original_still").getString("url");
-                int poc = img.indexOf("giphy");
-                int kraj = img.indexOf("?");
-                String link = "https://i." + img.substring(poc, kraj);
-                Button b = new Button();
-                ImageView view = new ImageView(new Image(link));
-                view.setFitHeight(128);
-                view.setFitWidth(128);
-                b.setGraphic(view);
-                tilePane.getChildren().add(b);
-            }
-        }  catch(MalformedURLException e) {
-            System.out.println("String "+adresa+" ne predstavlja validan URL");
-        } catch(IOException e) {
-            System.out.println("Greška pri kreiranju ulaznog toka");
-            System.out.println(e.getMessage());
-        } catch(Exception e) {
-            System.out.println("Poteškoće sa obradom JSON podataka");
-            System.out.println(e.getMessage());
-        }
+            Platform.runLater(() -> tilePane.getChildren().remove(25));
+        }).start();
     }
+
 }
