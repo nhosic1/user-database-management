96156c0606ff7bf280e14f856b6f031818af3233
nedim <https://github.com/nhosic1>
2021-01-17 21:08:08 +0100

    prikazan rezultat pretrage(blokiran interfejs)

--- src/ba/unsa/etf/rpr/t7/PretragaController.java
+++ src/ba/unsa/etf/rpr/t7/PretragaController.java
@@ -2,8 +2,11 @@ package ba.unsa.etf.rpr.t7;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,7 +20,7 @@ import java.nio.charset.StandardCharsets;
 
 public class PretragaController {
     public TextField fldPretraga;
-    public ScrollPane scrollPane;
+    public TilePane tilePane;
 
     public void cancel(ActionEvent actionEvent) {
         Node node = (Node)actionEvent.getSource();
@@ -26,22 +29,31 @@ public class PretragaController {
     }
 
     public void search(ActionEvent actionEvent) {
-        String adresa = "http://api.giphy.com/v1/gifs/search?q=cat&api_key=K7ZzpHSODkIauWCy4wi0qgypKMpicFX5&limit=5";
-        //String adresa = "https://giphy.com/search/cat";
+        String pretraga = fldPretraga.getText().replace(" ", "+");
+        String adresa = "http://api.giphy.com/v1/gifs/search?q=" + pretraga + "&api_key=K7ZzpHSODkIauWCy4wi0qgypKMpicFX5&limit=25";
+
         try{
             URL url = new URL(adresa);
             BufferedReader ulaz = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
             String json = "", line = null;
             while ((line = ulaz.readLine()) != null) {
                 json = json + line;
-                System.out.println(line);
             }
             ulaz.close();
             JSONObject obj = new JSONObject(json);
             JSONArray jArray = obj.getJSONArray("data");
             for(int i=0; i<jArray.length(); i++){
                 JSONObject o = jArray.getJSONObject(i);
-                System.out.println(o.getJSONObject("images").getJSONObject("original_still").getString("url"));
+                String img = o.getJSONObject("images").getJSONObject("original_still").getString("url");
+                int poc = img.indexOf("giphy");
+                int kraj = img.indexOf("?");
+                String link = "https://i." + img.substring(poc, kraj);
+                Button b = new Button();
+                ImageView view = new ImageView(new Image(link));
+                view.setFitHeight(128);
+                view.setFitWidth(128);
+                b.setGraphic(view);
+                tilePane.getChildren().add(b);
             }
         }  catch(MalformedURLException e) {
             System.out.println("String "+adresa+" ne predstavlja validan URL");
